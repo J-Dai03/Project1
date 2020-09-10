934554470e968faa8ebba6129bdc967e99e768d9
James Dai <K038246@eltham-college.org.uk>
2020-09-10 12:45:44 +0100

    10/09/2020 work

--- Program.cs
+++ Program.cs
@@ -7,10 +7,16 @@ namespace Project1
         static void Main(string[] args)
         {
             string SPH, Result = "";
-            int Num = 0, i = 1, IPH;
+            int Num = 0, i = 1, IPH = 0;
             float Returned = 2;
+            double DPH;
+
+            Console.WriteLine(Convert.ToString(DecToHexChar('1')))
+
             Console.WriteLine("Input denary number to convert to hexadecimal:");
             SPH = Console.ReadLine();
+
+
             try
             {
                 Num = Convert.ToInt32(SPH);
@@ -19,16 +25,35 @@ namespace Project1
             {
                 Console.WriteLine("Error: Invalid input");
             }
-            Returned = Num / 16;
+
+
             try
             {
                 SPH = Convert.ToString(IPH);
             }
-            while (Returned => 1);
+            catch
+            {
+                Console.WriteLine("Unknown Error");
+            }
+
+
+
+            do
             {
                 Result = SPH + Result;
-                IPH = Math.Pow(16,i)
-            }
+                DPH = Math.Pow((Convert.ToDouble(16)), Convert.ToDouble(i));
+            } while (Returned >= 1.0);
         }
+
+        static char DecToHexChar(char x)
+        {
+            char HexChar = '1';
+            char[] characters;
+            characters = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+            HexChar = characters[x-1];
+            return HexChar;
+
+        }
+
     }
 }
